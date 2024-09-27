67f699563ea173c3d8e3e554373cc208b61d00f1
victoriaag6 <victoriaacostag6@gmail.com>
2024-09-27 03:30:29 -0400

    iniciar proyecto

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import icon from './assets/icon.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={icon} style={{width: 100, height: 100}}/>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
