9230a1a2df2e7fa83bb65870c7b1e27270d208e7
victoriaag6 <victoriaacostag6@gmail.com>
2024-09-27 22:33:32 -0400

    empezando 1

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
-import icon from './assets/icon.png';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import fondo from './assets/fondo.jpg'; // Asegúrate de que la imagen exista en esta ruta
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source={icon} style={{width: 100, height: 100}}/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={fondo} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.text}>DAILY DIARIES</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#09f',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    padding: 20,
+    alignItems: 'center',
+    marginTop: -470, // Ajusta el valor según lo que necesites
+  },
+  text: {
+    color: 'white', 
+    fontSize: 40,
+    fontFamily: 'Garet',
   },
 });
